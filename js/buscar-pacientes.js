15ae0232a78c00b85e2acf71509b1e8bac29f56b
Rafael <tuissirafael@gmail.com>
2020-12-02 22:19:10 -0300

    Exibindo mensagem de erro em caso de falha na requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -3,10 +3,13 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener("click", function() {
 	var xhr = new XMLHttpRequest();
 
-	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/paentes");
 
 	xhr.addEventListener("load", function(){
-		
+		var erroAjax = document.querySelector("#erro-ajax");
+
+	if (xhr.status == 200) {
+		erroAjax.classList.add("invisivel");
 		var resposta = xhr.responseText;
 		var pacientes = JSON.parse(resposta);
 
@@ -14,6 +17,11 @@ botaoAdicionar.addEventListener("click", function() {
 			adicionaPacienteNaTabela(paciente);
 
 		});
+
+	} else {
+		erroAjax.classList.remove("invisivel");
+
+	}
 
 	});
 
